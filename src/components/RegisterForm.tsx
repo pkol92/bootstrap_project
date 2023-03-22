d0de7dad6dc3d2e59362395fb9595a11f816ba09
Paulina <paulina.kolasa@espeo.eu>
2023-03-22 16:00:25 +0100

    add required

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import { americanStates } from '../mocks/americanStates';
 
 export const RegisterForm = () => {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
   return (
-    <Form className='m-md-3 mt-md-5 p-4'>
+    <Form
+      className='m-md-3 mt-md-5 p-4'
+      noValidate
+      validated={validated}
+      onSubmit={handleSubmit}>
       <h3 className='mb-4'>Register Form</h3>
       <hr />
       <Row className='mb-3'>
@@ -20,7 +35,10 @@ export const RegisterForm = () => {
               controlId='formGridName'
               className='mb-3'>
               <Form.Label>First Name</Form.Label>
-              <Form.Control placeholder='John' type='text' />
+              <Form.Control required placeholder='John' type='text' />
+              <Form.Control.Feedback type='invalid'>
+                First name is required
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group
               as={Col}
@@ -29,7 +47,10 @@ export const RegisterForm = () => {
               controlId='formGridLastName'
               className='mb-3'>
               <Form.Label>Last name</Form.Label>
-              <Form.Control placeholder='Doe' type='text' />
+              <Form.Control required placeholder='Doe' type='text' />
+              <Form.Control.Feedback type='invalid'>
+                Last name is required
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group as={Col} controlId='ControlTextarea' className='mb-3'>
               <Form.Label>About you</Form.Label>
@@ -53,28 +74,40 @@ export const RegisterForm = () => {
               controlId='formGridAddress1'
               className='mb-3'>
               <Form.Label>Address</Form.Label>
-              <Form.Control placeholder='1234 Main St' />
+              <Form.Control required placeholder='1234 Main St' />
+              <Form.Control.Feedback type='invalid'>
+                Address name is required
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group as={Col} xs={12} md={6} controlId='formGridState'>
               <Form.Label>State</Form.Label>
-              <Form.Select defaultValue='Choose...'>
+              <Form.Select defaultValue='Choose...' required>
                 {Object.entries(americanStates).map(([key, value]) => (
                   <option key={key}>{value}</option>
                 ))}
               </Form.Select>
+              <Form.Control.Feedback type='invalid'>
+                State is required
+              </Form.Control.Feedback>
             </Form.Group>
           </Row>
 
           <Row className='mb-3'>
             <Form.Group as={Col} xs={12} md={6} controlId='formGridCity'>
               <Form.Label>City</Form.Label>
-              <Form.Control placeholder='Los Angeles' />
+              <Form.Control required placeholder='Los Angeles' />
+              <Form.Control.Feedback type='invalid'>
+                City is required
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group as={Col} xs={12} md={6} controlId='formGridZip'>
               <Form.Label>Zip</Form.Label>
-              <Form.Control type='text' pattern='[0-9]*' />
+              <Form.Control required type='text' pattern='[0-9]*' />
+              <Form.Control.Feedback type='invalid'>
+                Zip is required
+              </Form.Control.Feedback>
             </Form.Group>
           </Row>
         </Col>
@@ -87,7 +120,10 @@ export const RegisterForm = () => {
         <Col>
           <Form.Group as={Col} controlId='formBasicEmail' className='mb-3'>
             <Form.Label>Email address</Form.Label>
-            <Form.Control type='email' placeholder='Enter email' />
+            <Form.Control required type='email' placeholder='Enter email' />
+            <Form.Control.Feedback type='invalid'>
+              Email is required
+            </Form.Control.Feedback>
             <Form.Text className='text-muted'>
               We will never share your email with anyone else.
             </Form.Text>
@@ -95,13 +131,17 @@ export const RegisterForm = () => {
 
           <Form.Group as={Col} controlId='formBasicPassword' className='mb-3'>
             <Form.Label>Password</Form.Label>
-            <Form.Control type='password' placeholder='Password' />
+            <Form.Control required type='password' placeholder='Password' />
+            <Form.Control.Feedback type='invalid'>
+              Password is required
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group as={Col} controlId='formBasicCheckbox'>
             <Form.Check
               type='checkbox'
               label='I read the regulations and accept the conditions'
+              required
             />
           </Form.Group>
         </Col>
